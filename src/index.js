c22c2de6fd7bcca7cb828b9ade574839e14815c4
kawaji michihiro <michikun06@icloud.com>
2020-07-06 12:29:46 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import './index.css';
-import App from './App';
+import reducer from 'reducers';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
+
+
+// storeはアプリケーション内部で一つだけ作成し、全ての状態をこのstoreに集約させる
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
