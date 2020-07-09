c240f2f91447faf084c7f07abcfb6dbc0222cebf
kawaji michihiro <michikun06@icloud.com>
2020-07-09 16:17:11 +0900

    implement delete feature

--- src/action/index.js
+++ src/action/index.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 // actionはJavaScriptオブジェクトの1つで内部に「type」とtypeに対応する「値」を持っている
 export const READ_EVENTS = 'READ_EVENTS'
+export const READ_EVENT = 'READ_EVENT'
 export const CREATE_EVENT = 'CREATE_EVENT'
+export const DELETE_EVENT = 'DELETE_EVENT'
+export const UPDATE_EVENT = 'UPDATE_EVENT'
 
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
@@ -31,5 +34,29 @@ export const postEvent = values => async dispatch => {
 
     // reducerにtype,responseをdispatchする
     dispatch({ type: CREATE_EVENT, response })
+}
+
+
+export const putEvent = values => async dispatch => {
+    const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERYSTRING}`, values)
+    dispatch({ type: UPDATE_EVENT, response })
+}
+
+
+// 
+export const getEvent = id => async dispatch => {
+    const response = await axios.get(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+    dispatch({ type: READ_EVENT, response })
+}
+
+
+// event更新画面から選択されたイベントのidを元に削除するための要求を行う
+export const deleteEvent = id => async dispatch => {
+
+    // 入力されたtitleとbodyを送信
+    await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+
+    // reducerにtype,responseをdispatchする
+    dispatch({ type: DELETE_EVENT, id })
 }
 
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,14 +1,37 @@
 import _ from 'lodash';
-import { READ_EVENTS } from "../action";
+import {
+    CREATE_EVENT,
+    READ_EVENTS,
+    READ_EVENT,
+    UPDATE_EVENT,
+    DELETE_EVENT,
+} from "../action";
 
 // actionから受け取ったtypeを適応してstateを更新する。
 export default (events = {}, action) => {
     switch (action.type) {
 
+        case CREATE_EVENT:
+        case READ_EVENT:
+        case UPDATE_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
+
         // 初期状態（componentDidMountで呼び出した）の外部からevent一覧情報を読み込むための関数
         case READ_EVENTS:
             // action/index.jsから参照したaction.response.data内にあるidをkeyに設定する
             return _.mapKeys(action.response.data, 'id')
+
+
+        // 指定したidを削除する関数
+        case DELETE_EVENT:
+            // 指定したidを削除した状態でeventsオブジェクトを渡す
+            delete events[action.id]
+            // 削除後の更新したeventsを返す
+            return { ...events }
+
+
+        //　変更前の状態を返す
         default:
             return events
     }
